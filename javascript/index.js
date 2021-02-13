1753650974db5478bcb51347e3059103b0378cc3
Kushal <agrawalskushal@gmail.com>
2021-02-13 14:51:11 +0530

    All rows complete.

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,27 @@
-const apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1";
+console.log("Check");
+const apiArray=["https://api.themoviedb.org/3/movie/popular?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1","https://api.themoviedb.org/3/movie/latest?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1","https://api.themoviedb.org/3/movie/top_rated?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1","https://api.themoviedb.org/3/movie/upcoming?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1"]
 const imgPath = "https://image.tmdb.org/t/p/w154"
-async function getMovies(){
+async function getMovies(apiUrl,id){
   const resp = await fetch(apiUrl);
   const respData = await resp.json();
   console.log(respData);
   respData.results.forEach((movie, i) => {
     const movieInfo = document.createElement(`div`);
     movieInfo.classList.add(`movie`);
-    movieInfo.innerHTML=
+    movieInfo.innerHTML= `
     <div class="image">
-      <img src= `${imgPath + movie.poster_path}`/>
+      <img src= "${imgPath + movie.poster_path}"/>
     </div>
     <div class = "movie-info">
       <div class = "movie-name">
-        `${movie.title}`
+        "${movie.title}"
       </div>
       <div class = "movie-rating">
-        `${movie.vote_average}`
-      </div>;
-    document.getElementByClassName(main).appendChild(movieInfo);
+        ${movie.vote_average}
+      </div>`;
+    document.getElementById(id).appendChild(movieInfo);
   });
-  return respData;
 }
-getMovies();
+getMovies(apiArray[0],"popular-movies");
+getMovies(apiArray[2],"top-rated-movies");
+getMovies(apiArray[3],"upcoming-movies");
