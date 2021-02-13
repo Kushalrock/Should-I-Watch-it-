94bf8dfd0cd4342e76bc56560a4733d72db0fbb1
Kushal <agrawalskushal@gmail.com>
2021-02-13 13:10:09 +0530

    Added the first row of popular movies

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,24 @@
 const apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1";
-const imgPath = "https://image.tmdb.org/t/p/w500"
+const imgPath = "https://image.tmdb.org/t/p/w154"
 async function getMovies(){
   const resp = await fetch(apiUrl);
   const respData = await resp.json();
   console.log(respData);
   respData.results.forEach((movie, i) => {
-    const movie_poster = movie.poster_path;
-    const img = document.createElement('img');
-    img.src = imgPath + movie_poster;
-    document.body.appendChild(img);
+    const movieInfo = document.createElement(`div`);
+    movieInfo.classList.add(`movie`);
+    movieInfo.innerHTML=
+    <div class="image">
+      <img src= `${imgPath + movie.poster_path}`/>
+    </div>
+    <div class = "movie-info">
+      <div class = "movie-name">
+        `${movie.title}`
+      </div>
+      <div class = "movie-rating">
+        `${movie.vote_average}`
+      </div>;
+    document.getElementByClassName(main).appendChild(movieInfo);
   });
   return respData;
 }
