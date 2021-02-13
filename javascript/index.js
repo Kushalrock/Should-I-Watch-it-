00339ba75cf9fee1829468ce103608464d1da625
Kushal <agrawalskushal@gmail.com>
2021-02-13 20:07:24 +0530

    Implemented the search funcntionality

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,9 @@
 console.log("Check");
-const apiArray=["https://api.themoviedb.org/3/movie/popular?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1","https://api.themoviedb.org/3/search/movie?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1&include_adult=true","https://api.themoviedb.org/3/movie/top_rated?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1","https://api.themoviedb.org/3/movie/upcoming?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1"]
+const apiArray=["https://api.themoviedb.org/3/movie/popular?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1","https://api.themoviedb.org/3/search/movie?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1&include_adult=true&query=","https://api.themoviedb.org/3/movie/top_rated?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1","https://api.themoviedb.org/3/movie/upcoming?api_key=802f2cc35253cb99efe6d36dc71ba39d&language=en-US&page=1"]
 const imgPath = "https://image.tmdb.org/t/p/w154"
+const form = document.querySelector("#form");
+const search = document.querySelector(".search");
+
 async function getMovies(apiUrl,id){
   const resp = await fetch(apiUrl);
   const respData = await resp.json();
@@ -10,7 +13,7 @@ async function getMovies(apiUrl,id){
     movieInfo.classList.add(`movie`);
     movieInfo.innerHTML= `
     <div class="image">
-      <img src= "${imgPath + movie.poster_path}"/>
+      <img src= "${imgPath + movie.poster_path}" alt="${movie.title}"/>
     </div>
     <div class = "movie-info">
       <div class = "movie-name">
@@ -21,9 +24,6 @@ async function getMovies(apiUrl,id){
       </div>`;
     document.getElementById(id).appendChild(movieInfo);
   });
-}
-function submitHandler(){
-  console.log("Green Signal");
 }
 function getRatingColor(rating){
   if(rating >= 7){
@@ -36,6 +36,23 @@ function getRatingColor(rating){
     return "red";
   }
 }
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  const searchTerm = search.value;
+  document.body.innerHTML= `
+  <header>
+    <div class="nav">
+      <h2>Should I Watch It?</h2>
+      <form id="form">
+        <input type="text" name="search" placeholder="Search movies"/>
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </form>
+    </div>
+  </header>
+  <div id="search-result"></div>`;
+  getMovies(apiArray[1] + searchTerm,"search-result");
+})
 getMovies(apiArray[0],"popular-movies");
 getMovies(apiArray[2],"top-rated-movies");
 getMovies(apiArray[3],"upcoming-movies");
